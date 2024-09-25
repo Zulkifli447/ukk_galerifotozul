7aea5c8f38eed8f4497628e51ad09c620ea33564
Zulkifli447 <izull447@gmail.com>
2024-09-25 09:52:52 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -31,7 +31,7 @@ if ($_SESSION['status'] != 'login') {
 
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container">
-            <a class="navbar-brand" href="index.php">Galeri Foto</a>
+            <a class="navbar-brand" href="index.php">Website Galeri Foto</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
