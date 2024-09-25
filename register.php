d3916e4917232dba0af3a04a22bf506bb34ea7b8
Zulkifli447 <izull447@gmail.com>
2024-09-25 09:27:11 +0800

    Update register.php

--- register.php
+++ register.php
@@ -5,6 +5,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Website Galeri Foto</title>
   <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+  <style>
+    body {
+      background-image: url('assets/img/background.jpg');
+      background-size: cover;
+    }
+  </style>
 </head>
 <body>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
@@ -15,7 +21,7 @@
       </button>
       <div class="collapse navbar-collapse mt-2" id="navbarNavAltMarkup">
         <div class="navbar-nav me-auto">
-         
+
         </div>
         <a href="register.php" class="btn btn-outline-primary m-1">Daftar</a>
         <a href="login.php" class="btn btn-outline-success m-1">Masuk</a>
